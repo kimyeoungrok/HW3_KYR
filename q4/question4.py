45e67b0dc2a48e4356ee7821daa81150d95a20a6
kimyeoungrok <praoo800@konkuk.ac.kr>
2023-04-06 17:57:39 +0900

    secondmodified

--- q4/question4.py
+++ q4/question4.py
@@ -1,53 +1,39 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "b99c9d10",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def main():\n",
-    "    print(factorial(0))\n",
-    "    print(factorial(2))\n",
-    "    print(factorial(4))\n",
-    "    print(factorial(6))\n",
-    "    print(factorial(8))\n",
-    "    print(factorial(10))\n",
-    "    print(factorial(12))\n",
-    "    print(factorial(14))\n",
-    "def factorial(num):\n",
-    "    if num == 1 or num==0:\n",
-    "        return 1\n",
-    "    else:\n",
-    "        return num*factorial(num-1)\n",
-    "\n",
-    "    \n",
-    "    \n",
-    "if __name__ == '__main__':\n",
-    "    main()"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def main():
+    print(factorial(0))
+    print(factorial(2))
+    print(factorial(4))
+    print(factorial(6))
+    print(factorial(8))
+    print(factorial(10))
+    print(factorial(12))
+    print(factorial(14))
+        
+        
+
+    
+def factorial(num):
+    if num == 1 or num == 0:
+        return 1
+    else:
+        return num*factorial(num-1)
+    
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
+
